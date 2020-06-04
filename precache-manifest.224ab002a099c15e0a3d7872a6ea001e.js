64fa1314f1f4fae203e59543d2de63beb7eb49a9
Mehak Zehra <mehak.rizvi.786@gmail.com>
2020-06-04 01:29:09 -0700

    Updates

---
+++ precache-manifest.224ab002a099c15e0a3d7872a6ea001e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "849708a0a711684af58ff989f506f2d9",
+    "url": "/countries-app/index.html"
+  },
+  {
+    "revision": "3bc2398b72b45f5522c2",
+    "url": "/countries-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "4bc3272b6a1b34835fdc",
+    "url": "/countries-app/static/js/2.0f56d09a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/countries-app/static/js/2.0f56d09a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bc2398b72b45f5522c2",
+    "url": "/countries-app/static/js/main.85974fe3.chunk.js"
+  },
+  {
+    "revision": "5d2aee138ab7a614c354",
+    "url": "/countries-app/static/js/runtime-main.22ea0ad9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/countries-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
